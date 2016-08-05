c42cfe69555bd17040f998af04ffd423008fc3cf
ShevyakovDY <stormbringer-s@mail.ru>
2016-08-05 13:40:27 +0400

    correct namespaces

--- WebAPI/App_Start/Startup.Auth.cs
+++ WebAPI/App_Start/Startup.Auth.cs
@@ -8,8 +8,8 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
-using WebApplication1.Providers;
-using WebApplication1.Models;
+using WebAPI.Providers;
+using WebAPI.Models;
 
 namespace WebAPI
 {
